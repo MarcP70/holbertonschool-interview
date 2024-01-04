86781036b74f306cae54a138750ee337d734379a
MarcP70 <marcpourias@yahoo.fr>
2024-01-04 20:42:02 +0100

    add 0-lockboxes.py to determines if all the boxes can be opened

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+    Args:
+        boxes: List of list
+    Returns:
+        True or false
+    """
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    keys = set(boxes[0])
+    opened_boxes = {0}
+
+    while keys:
+        box_number = keys.pop()
+        if box_number < n and box_number not in opened_boxes:
+            opened_boxes.add(box_number)
+            keys.update(boxes[box_number])
+
+    return len(opened_boxes) == n
