efef349112b12aa1cdf5a5428ec2a9af06f8a7a5
MarcP70 <marcpourias@yahoo.fr>
2024-01-12 12:08:46 +0100

    fix 0-insert_number.c

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -10,32 +10,29 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current, *prev;
-
-    current = *head;
-    prev = NULL;
+    listint_t *new_node, *current_node;
 
     new_node = malloc(sizeof(listint_t));
     if (new_node == NULL)
         return (NULL);
 
     new_node->n = number;
-    new_node->next = NULL;
 
-    while (current != NULL && current->n < number) {
-        prev = current;
-        current = current->next;
-    }
-
-    if (prev == NULL) {
-        // Insertion au début de la liste
+    if (*head == NULL || (*head)->n > number)
+    {
         new_node->next = *head;
         *head = new_node;
-    } else {
-        // Insertion à un autre endroit
-        prev->next = new_node;
-        new_node->next = current;
+        return new_node;
     }
+
+    current_node = *head;
+    while (current_node->next != NULL && current_node->next->n < number)
+    {
+        current_node = current_node->next;
+    }
+
+    new_node->next = current_node->next;
+    current_node->next = new_node;
 
     return new_node;
 }
