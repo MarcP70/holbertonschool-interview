b224befeb8c63faea24d3ceb146b35042d05023c
MarcP70 <marcpourias@yahoo.fr>
2024-02-19 10:27:28 +0100

    fix stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,88 +1,45 @@
 #!/usr/bin/python3
 """
-This module reads stdin line by line and computes metrics.
+Reads stdin line by line and computes metrics
 """
-import sys
 
-
-def print_statistics(total_file_size, status_codes):
-    """
-    Fonction pour afficher les statistiques.
-
-    Args:
-        total_file_size (int): La taille totale des fichiers.
-        status_codes (dict): Un dictionnaire contenant les codes de statut HTTP
-                             comme clés et le nombre d'occurrences comme
-                                valeurs.
-    Returns:
-        None
-    """
-
-    print(f"File size: {total_file_size}")
-    for code, count in sorted(status_codes.items()):
-        if count > 0:
-            print(f"{code}: {count}")
-
-
-def main():
-    """
-    Fonction principale du programme.
-
-    Elle lit les logs du serveur à partir de l'entrée standard (stdin),
-    calcule la taille totale des fichiers demandés et compte le nombre
-    d'occurrences de chaque code de statut HTTP. Elle affiche ensuite
-    ces statistiques toutes les 10 lignes et à la fin du traitement
-    ou en cas d'interruption par l'utilisateur (Ctrl+C).
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-
-    try:
-        total_file_size = 0
-        status_codes = {
-            200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
-            }
-        line_count = 0  # Initialisation du compteur de lignes
-
-        for line in sys.stdin:
-            parts = line.split()
-            try:
-                status_code = int(parts[-2])  # Convertir en entier
-            except ValueError:
-                continue  # Ignorer les lignes avec un code de statut invalide
-            file_size = int(parts[-1])
-            total_file_size += file_size
-            status_codes[status_code] += 1
-
-            # Incrémenter le compteur de lignes
-            line_count += 1
-
-            # Afficher le total de la taille des fichiers et les codes de
-            # statut toutes les 10 lignes
-            if line_count % 10 == 0:
-                print_statistics(total_file_size, status_codes)
-
-        # Afficher le total final de la taille des fichiers et les codes de
-        # statut s'il reste des lignes
-        if line_count % 10 != 0:
-            print_statistics(total_file_size, status_codes)
-
-    except KeyboardInterrupt:
-        print_statistics(total_file_size, status_codes)
-        sys.exit(0)
-
+from sys import stdin
 
 if __name__ == "__main__":
-    main()
-
-"""
-Ce script lit les logs du serveur à partir de l'entrée standard (stdin),
-calcule la taille totale des fichiers demandés et compte le nombre d'
-occurrences de chaque code de statut HTTP. Il affiche ensuite ces statistiques
-toutes les 10 lignes et à la fin du traitement ou en cas d'interruption par l'
-utilisateur (Ctrl+C).
-"""
+    total_size = 0
+    status_codes = {}
+    list_status_codes = [
+        "200", "301", "400", "401", "403", "404", "405", "500"]
+    for status in list_status_codes:
+        status_codes[status] = 0
+    count = 0
+    try:
+        for line in stdin:
+            try:
+                args = line.split(" ")
+                if len(args) != 9:
+                    pass
+                if args[-2] in list_status_codes:
+                    status_codes[args[-2]] += 1
+                if args[-1][-1] == '\n':
+                    args[-1][:-1]
+                total_size += int(args[-1])
+            except:
+                pass
+            count += 1
+            if count % 10 == 0:
+                print("File size: {}".format(total_size))
+                for status in sorted(status_codes.keys()):
+                    if status_codes[status] != 0:
+                        print("{}: {}".format(
+                            status, status_codes[status]))
+        print("File size: {}".format(total_size))
+        for status in sorted(status_codes.keys()):
+            if status_codes[status] != 0:
+                print("{}: {}".format(status, status_codes[status]))
+    except KeyboardInterrupt as err:
+        print("File size: {}".format(total_size))
+        for status in sorted(status_codes.keys()):
+            if status_codes[status] != 0:
+                print("{}: {}".format(status, status_codes[status]))
+        raise
