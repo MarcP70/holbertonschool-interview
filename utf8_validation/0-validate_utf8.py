9e1e8da05753ea4528f8a0776f50419f737abcaf
MarcP70 <marcpourias@yahoo.fr>
2024-03-08 10:57:25 +0100

    fix 0-validate_utf-8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -15,16 +15,18 @@ def validUTF8(data):
 
     for byte in data:
         if num_bytes == 0:
-            if byte >> 5 == 0b110:
+            if byte >> 7 == 0:  # Single byte character (0xxxxxxx)
+                num_bytes = 0
+            elif byte >> 5 == 0b110:  # Two byte character (110xxxxx)
                 num_bytes = 1
-            elif byte >> 4 == 0b1110:
+            elif byte >> 4 == 0b1110:  # Three byte character (1110xxxx)
                 num_bytes = 2
-            elif byte >> 3 == 0b11110:
+            elif byte >> 3 == 0b11110:  # Four byte character (11110xxx)
                 num_bytes = 3
-            elif byte >> 7 == 1:
+            else:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if byte >> 6 != 0b10:  # Continuation byte (10xxxxxx)
                 return False
             num_bytes -= 1
 
