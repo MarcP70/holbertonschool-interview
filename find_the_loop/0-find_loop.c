2f92ed51f660ec66c4d4cdd7caac9be73fac1dbc
MarcP70 <marcpourias@yahoo.fr>
2024-10-11 09:38:08 +0200

    Apply betty style on file 0-find_loop.c

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -8,32 +8,32 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    /* Step 1: Use the tortoise and hare approach to detect a loop */
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	/* Step 1: Use the tortoise and hare approach to detect a loop */
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        /* If slow and fast meet, there is a loop */
-        if (slow == fast)
-        {
-            /* Step 2: Find the start of the loop */
-            slow = head;  /* Move slow pointer back to head */
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (slow);  /* The start of the loop */
-        }
-    }
+		/* If slow and fast meet, there is a loop */
+		if (slow == fast)
+		{
+			/* Step 2: Find the start of the loop */
+			slow = head;  /* Move slow pointer back to head */
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);  /* The start of the loop */
+		}
+	}
 
-    /* If no loop is detected, return NULL */
-    return (NULL);
+	/* If no loop is detected, return NULL */
+	return (NULL);
 }
